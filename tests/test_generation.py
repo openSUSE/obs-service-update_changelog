39846d7ffd043aa746d086b1126ca656fd863122
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2025-01-22 15:55:49 +0000

    Fix code format according to Black

--- tests/test_generation.py
+++ tests/test_generation.py
@@ -8,7 +8,7 @@ from updatechangelog import common
 
 def test_nothing_new():
     os_mock = Mock()
-    os_mock.getcwd.return_value = ''
+    os_mock.getcwd.return_value = ""
     Repo_mock = MagicMock()
     write_mock = Mock()
     open_mock = MagicMock()
@@ -21,20 +21,11 @@ def test_nothing_new():
 def test_rendering():
     template = common.get_template()
     messages = ["First entry", "Second entry"]
-    name = 'Fake Name'
-    email = 'fake@example.com'
-    added = [
-        'first_added.patch',
-        'second_added.patch'
-    ]
-    modified = [
-        'first_modified.patch',
-        'second_modified.patch'
-    ]
-    deleted = [
-        'first_deleted.patch',
-        'second_deleted.patch'
-    ]
+    name = "Fake Name"
+    email = "fake@example.com"
+    added = ["first_added.patch", "second_added.patch"]
+    modified = ["first_modified.patch", "second_modified.patch"]
+    deleted = ["first_deleted.patch", "second_deleted.patch"]
     current_dt = datetime.now().replace(tzinfo=pytz.utc)
 
     changelog_entry = template.render(
@@ -43,40 +34,33 @@ def test_rendering():
         modified=modified,
         deleted=deleted,
     ).strip()
-    expected = ''
-    with open('tests/expected.output', 'r') as f:
+    expected = ""
+    with open("tests/expected.output", "r") as f:
         expected = f.read().strip()
     assert changelog_entry == expected
 
 
 def test_new_commit():
     os_mock = Mock()
-    os_mock.getcwd.return_value = ''
+    os_mock.getcwd.return_value = ""
     Repo_mock = MagicMock()
     write_mock = Mock()
     open_mock = MagicMock()
     open_mock.write = write_mock
 
     commit1_mock = MagicMock()
-    commit1_mock.configure_mock(
-        hexsha = 'abcdef12345'
-    )
+    commit1_mock.configure_mock(hexsha="abcdef12345")
     commit1_mock.tree.side_effect = [
-        {'salt': [Mock(path='existing.patch')]},
+        {"salt": [Mock(path="existing.patch")]},
     ]
 
     commit2_mock = MagicMock()
-    commit2_mock.configure_mock(
-        hexsha = 'abcdef12345'
-    )
+    commit2_mock.configure_mock(hexsha="abcdef12345")
     commit2_mock.tree.side_effect = [
-        {'salt': [Mock(path='current.patch')]},
+        {"salt": [Mock(path="current.patch")]},
     ]
 
-    Repo_mock.return_value.commit.side_effects = [
-        commit1_mock,
-        commit2_mock
-    ]
+    Repo_mock.return_value.commit.side_effects = [commit1_mock, commit2_mock]
 
     with patch.multiple(common, os=os_mock, Repo=Repo_mock, open=open_mock):
         common.main()
